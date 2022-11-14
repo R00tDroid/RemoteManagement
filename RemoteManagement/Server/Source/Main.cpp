3d90f2f480a21f26f19202a7de323dc35000f5a9
R00tDroid <info@rootdroid.nl>
2022-11-14 21:36:17 +0100

    Serve index

--- RemoteManagement/Server/Source/Main.cpp
+++ RemoteManagement/Server/Source/Main.cpp
@@ -1,9 +1,22 @@
 #include <cmrc/cmrc.hpp>
+#include <restinio/all.hpp>
 
 CMRC_DECLARE(ServerResources);
 
 int main()
 {
     auto ResourceFS = cmrc::ServerResources::get_filesystem();
+
+    restinio::run(
+    restinio::on_this_thread()
+    .port(8080)
+    .address("localhost")
+    .request_handler([ResourceFS](auto Request) 
+    {
+        auto File = ResourceFS.open("Resources/index.html");
+        std::string FileData(File.begin(), File.end());
+        return Request->create_response().set_body(FileData).done();
+    }));
+
     return 0;
 }
