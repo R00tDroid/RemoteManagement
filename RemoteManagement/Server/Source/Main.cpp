179e523ab86f8e7878adfa9e8f26cd53a5218d2b
R00tDroid <info@rootdroid.nl>
2022-11-15 17:13:29 +0100

    Serve file dynamically from resource filesystem

--- RemoteManagement/Server/Source/Main.cpp
+++ RemoteManagement/Server/Source/Main.cpp
@@ -1,22 +1,61 @@
 #include <cmrc/cmrc.hpp>
 #include <restinio/all.hpp>
+#include <filesystem>
 
 CMRC_DECLARE(ServerResources);
 
+cmrc::embedded_filesystem* ResourceFS = nullptr;
+
+restinio::request_handling_status_t handler(restinio::request_handle_t Request)
+{
+    if (restinio::http_method_get() == Request->header().method())
+    {
+        std::string TargetResource = Request->header().request_target();
+
+        if (TargetResource == "/")
+        {
+            TargetResource += "index.html";
+        }
+
+        TargetResource = "Resources/" + TargetResource;
+
+        if (ResourceFS->exists(TargetResource))
+        {
+            cmrc::file File = ResourceFS->open(TargetResource);
+            std::string FileData(File.begin(), File.end());
+
+            std::string Extension = std::filesystem::path(TargetResource).extension().string();
+            std::string ContentType = "";
+
+            if (Extension == ".html")
+            {
+                ContentType = "text/html; charset=utf-8";
+            }
+
+            auto Response = Request->create_response();
+            Response.append_header(restinio::http_field::server, "RemoteManagement WebInterface");
+            Response.append_header_date_field();
+            Response.append_header(restinio::http_field::content_type, ContentType);
+            Response.set_body(FileData);
+            Response.done();;
+
+            return restinio::request_accepted();
+        }
+    }
+
+    return restinio::request_rejected();
+}
+
 int main()
 {
-    auto ResourceFS = cmrc::ServerResources::get_filesystem();
+    cmrc::embedded_filesystem FS = cmrc::ServerResources::get_filesystem();
+    ResourceFS = &FS;
 
     restinio::run(
-    restinio::on_this_thread()
-    .port(8080)
-    .address("localhost")
-    .request_handler([ResourceFS](auto Request) 
-    {
-        auto File = ResourceFS.open("Resources/index.html");
-        std::string FileData(File.begin(), File.end());
-        return Request->create_response().set_body(FileData).done();
-    }));
+        restinio::on_this_thread()
+        .port(8080)
+        .address("localhost")
+        .request_handler(handler));
 
     return 0;
 }
